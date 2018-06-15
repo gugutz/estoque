86d386d3525962b0ba19eb10b99caf62e87c8cd4
tau <gugutz@gmail.com>
2018-06-15 20:37:54 -0300

    attempt at fetching db data and populate tableview

--- src/main/java/controller/ListItems.java
+++ src/main/java/controller/ListItems.java
@@ -1,13 +1,24 @@
 package controller;
 
 import db.DB;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class ListItems {
+    @FXML private TableView<User> Products;
+    @FXML private TableColumn<User, String> productIdColumn;
+    @FXML private TableColumn<User, String> productNameColumn;
+    @FXML private TableColumn<User, String> productDescriptionColumn;
+
 
     private static final String QUERY = "SELECT id,descricao,linha from perfis ORDER DESC";
 
@@ -19,10 +30,46 @@ public class ListItems {
             Statement stmt = con.createStatement();
             ResultSet results = stmt.executeQuery(QUERY);
 
+
+        while(results.next()){
             int id = results.getInt("id");
             String name = results.getString("descricao");
-            String email = results.getString("linha");
-            System.out.println(id + "," +name+ "," +email  );
+            String description = results.getString("linha");
+
+            productIdColumn.setCellValueFactory(new PropertyValueFactory<User, String>("id"));
+            productNameColumn.setCellValueFactory(new PropertyValueFactory<User, String>("name"));
+            productDescriptionColumn.setCellValueFactory(new PropertyValueFactory<User, String>("active"));
+
+            tableView.getItems().setAll(parseUserList());
+
+            System.out.println(id + "," +name+ "," +description+ ",");
+            }
+
+
+                // begin stack overflow code
+                List<Integer> intValues = Arrays.asList(1, 2, 3, 4, 5);
+                List<String> stringValues = Arrays.asList("One", "Two", "Three", "Four", "Five");
+
+                TableView<Integer> table = new TableView<>();
+                for (int i = 0; i < intValues.size() && i < stringValues.size(); i++) {
+                    table.getItems().add(i);
+                }
+
+                TableColumn<Integer, Number> intColumn = new TableColumn<>("Value");
+                intColumn.setCellValueFactory(cellData -> {
+                    Integer rowIndex = cellData.getValue();
+                    return new ReadOnlyIntegerWrapper(intValues.get(rowIndex));
+                });
+
+                TableColumn<Integer, String> nameColumn = new TableColumn<>("Name");
+                nameColumn.setCellValueFactory(cellData -> {
+                    Integer rowIndex = cellData.getValue();
+                    return new ReadOnlyStringWrapper(stringValues.get(rowIndex));
+                });
+
+                table.getColumns().add(intColumn);
+                table.getColumns().add(nameColumn);
+                // end of stackoverflow code
 
         } catch (SQLException e) {
             System.out.println("conexao falhou");
@@ -30,3 +77,4 @@ public class ListItems {
         }
     }
 }
+
